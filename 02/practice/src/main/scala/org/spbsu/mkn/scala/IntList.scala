9808c21e3e8a0e988a790ff900da6d1d74122b4a
ainurbl <ainur19dota2@mail.ru>
2021-02-18 18:25:25 +0300

    second task is done

--- 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
+++ 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
@@ -3,20 +3,53 @@ package org.spbsu.mkn.scala
 import org.spbsu.mkn.scala.IntList._
 
 sealed trait IntList {
-  def i: Int
-  def head: IntList
-  def tail: IntList
+  def head: Int
+
+  def tail: IntList = drop(1)
+
   def drop(n: Int): IntList
+
   def take(n: Int): IntList
+
   def map(f: Int => Int): IntList
-  def ::(elem: Int): IntList = ???
+
+  def ::(elem: Int): IntList = new ::(elem, this)
 }
 
 object IntList {
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): IntList = ???
-  def sum(intList: IntList): Int      = ???
-  def size(intList: IntList): Int     = ???
-  // extra task: implement sum using foldLeft
-  // def foldLeft(???)(???): ??? = ???
+
+  def fromSeq(seq: Seq[Int]): IntList = seq.foldRight[IntList](IntNil)((elem: Int, xs: IntList) => elem :: xs)
+
+  def sum(intList: IntList): Int = intList match {
+    case IntNil => undef
+    case a => foldLeft((calculated: Int, init: Int) => calculated + init)(a)
+  }
+
+  def size(intList: IntList): Int = foldLeft((_: Int, calculated: Int) => calculated + 1)(intList)
+
+  def foldLeft(f: (Int, Int) => Int)(intList: IntList): Int = intList match {
+    case IntNil => 0
+    case x :: xs => f(x, 0) + foldLeft(f)(xs)
+  }
+}
+
+case object IntNil extends IntList {
+  override def head: Int = undef
+
+  override def drop(n: Int): IntList = if (n == 0) this else undef
+
+  override def take(n: Int): IntList = if (n == 0) this else undef // possible simplification is take = drop, but is it OK?
+
+  override def map(f: Int => Int): IntList = this
+}
+
+case class ::(x: Int, xs: IntList) extends IntList {
+  override def head: Int = x
+
+  override def drop(n: Int): IntList = if (n == 0) this else xs drop n - 1
+
+  override def take(n: Int): IntList = if (n == 0) IntNil else x :: (xs take n - 1)
+
+  override def map(f: Int => Int): IntList = f(x) :: (xs map f)
 }
